2e110b61797d15de022491e02c63cf376e320963
Bảo <ngbao161199@gmail.com>
2021-01-17 01:49:47 +0700

    webp

---
+++ webpc.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import PIL
+import os
+import glob
+
+def convert_image(image_path, image_type):
+
+    # 1. Opening the image:
+    im = Image.open(image_path)
+    # 2. Converting the image to RGB colour:
+    im = im.convert('RGB')
+    # 3. Spliting the image path (to avoid the .jpg or .webp being part of the image name):
+    image_name = image_path.split('.')[0]
+    print(f"This is the image name: {image_name}")
+
+    # Saving the images based upon their specific type:
+    if image_type == 'jpg' or image_type == 'webp':
+        im.save(f"{image_name}.webp", 'webp')
+    else:
+        # Raising an error if we didn't get a jpeg or webp file type!
+        raise Error
+
+files = os.listdir() # We list all of the files and folders using os.listdir()
+
+images = [file for file in files if file.endswith(('jpg', 'webp'))]
+
+for image in images:
+    if image.endswith('jpg'):
+        convert_image(image, image_type='jpg')
+    elif image.endswith('webp'):
+        convert_image(image, image_type='webp')
+    else:
+        raise Error
